13da20c5bf6385b16d368a70a3b012f10d3b8e1e
Julian Bissekkou <julian.bissekkou@tapped.dev>
2020-11-09 07:00:24 +0100

    parse brand name if type is Brand

--- src/main/java/com/fatsecret/platform/utils/FoodUtility.java
+++ src/main/java/com/fatsecret/platform/utils/FoodUtility.java
@@ -33,101 +33,103 @@ import com.fatsecret.platform.model.CompactFood;
  */
 public class FoodUtility {
 
-	/**
-	 * Returns detailed information about the food
-	 * 
-	 * @param json			json object representing of the food
-	 * @return				detailed information about the food
-	 */
-	public static Food parseFoodFromJSONObject(JSONObject json) {
-		String name = json.getString("food_name");
-		String url = json.getString("food_url");
-		String type = json.getString("food_type");
-		Long id = Long.parseLong(json.getString("food_id"));
-		String brandName = "";
-		
-		try {
-			brandName = json.getString("brand_name");
-		} catch(Exception ignore) {
-		}
+    /**
+     * Returns detailed information about the food
+     *
+     * @param json json object representing of the food
+     * @return detailed information about the food
+     */
+    public static Food parseFoodFromJSONObject(JSONObject json) {
+        String name = json.getString("food_name");
+        String url = json.getString("food_url");
+        String type = json.getString("food_type");
+        Long id = Long.parseLong(json.getString("food_id"));
+        String brandName = "";
 
-		JSONObject servingsObj = json.getJSONObject("servings");
-		
-		JSONArray array = null;
-		List<Serving> servings = new ArrayList<Serving>();
-		
-		try {
-			array = servingsObj.getJSONArray("serving");
-			servings = ServingUtility.parseServingsFromJSONArray(array);
-		} catch(Exception ignore) {
-			System.out.println("Servings not found");
-			array = null;
-		}
-		
-		if(array == null) {
-			try {
-				JSONObject servingObj = servingsObj.getJSONObject("serving");
-				Serving serving = ServingUtility.parseServingFromJSONObject(servingObj);
-				servings.add(serving);
-			} catch(Exception ignore) {
-				System.out.println("Serving not found");
-			}
-		}
+        try {
+            brandName = json.getString("brand_name");
+        } catch (Exception ignore) {
+        }
 
-		Food food = new Food();
-		
-		food.setName(name);
-		food.setUrl(url);
-		food.setType(type);
-		food.setId(id);
-		food.setBrandName(brandName);
-		food.setServings(servings);
-		
-		return food;
-	}
-	
-	/**
-	 * Returns information about the compact food
-	 * 
-	 * @param json			json object representing of the food
-	 * @return				compact food object from the json
-	 */
-	public static CompactFood parseCompactFoodFromJSONObject(JSONObject json) {
-		
-		String name = json.getString("food_name");
-		String url = json.getString("food_url");
-		String type = json.getString("food_type");
-		String description = json.getString("food_description");
-		Long id = Long.parseLong(json.getString("food_id"));
+        JSONObject servingsObj = json.getJSONObject("servings");
 
-		CompactFood food = new CompactFood();
-		
-		food.setName(name);
-		food.setUrl(url);
-		food.setType(type);
-		food.setDescription(description);
-		food.setId(id);
-		
-		return food;
-	}
+        JSONArray array = null;
+        List<Serving> servings = new ArrayList<Serving>();
 
-	/**
-	 * Returns a list of compact food items
-	 * 
-	 * @param array			json array representing a list of compact food
-	 * @return				list of compact food items
-	 */
-	public static List<CompactFood> parseCompactFoodListFromJSONArray(JSONArray array) {
-		List<CompactFood> foods = new ArrayList<CompactFood>();
-		
-		for(int i = 0; i < array.length(); i++) {
-			JSONObject obj = array.getJSONObject(i);
-			
-			CompactFood food = parseCompactFoodFromJSONObject(obj);
-			
-			foods.add(food);
-		}
-		
-		return foods;
-	}
+        try {
+            array = servingsObj.getJSONArray("serving");
+            servings = ServingUtility.parseServingsFromJSONArray(array);
+        } catch (Exception ignore) {
+            System.out.println("Servings not found");
+            array = null;
+        }
+
+        if (array == null) {
+            try {
+                JSONObject servingObj = servingsObj.getJSONObject("serving");
+                Serving serving = ServingUtility.parseServingFromJSONObject(servingObj);
+                servings.add(serving);
+            } catch (Exception ignore) {
+                System.out.println("Serving not found");
+            }
+        }
+
+        Food food = new Food();
+
+        food.setName(name);
+        food.setUrl(url);
+        food.setType(type);
+        food.setId(id);
+        food.setBrandName(brandName);
+        food.setServings(servings);
+
+        return food;
+    }
+
+    /**
+     * Returns information about the compact food
+     *
+     * @param json json object representing of the food
+     * @return compact food object from the json
+     */
+    public static CompactFood parseCompactFoodFromJSONObject(JSONObject json) {
+
+        String name = json.getString("food_name");
+        String url = json.getString("food_url");
+        String type = json.getString("food_type");
+        String description = json.getString("food_description");
+        Long id = Long.parseLong(json.getString("food_id"));
+        String brandName = type.equals("Brand") ? json.getString("brand_name") : null;
+
+        CompactFood food = new CompactFood();
+
+        food.setName(name);
+        food.setUrl(url);
+        food.setType(type);
+        food.setDescription(description);
+        food.setId(id);
+        food.setBrandName(brandName);
+
+        return food;
+    }
+
+    /**
+     * Returns a list of compact food items
+     *
+     * @param array json array representing a list of compact food
+     * @return list of compact food items
+     */
+    public static List<CompactFood> parseCompactFoodListFromJSONArray(JSONArray array) {
+        List<CompactFood> foods = new ArrayList<CompactFood>();
+
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject obj = array.getJSONObject(i);
+
+            CompactFood food = parseCompactFoodFromJSONObject(obj);
+
+            foods.add(food);
+        }
+
+        return foods;
+    }
 }
--- src/test/java/com/fatsecret/platform/services/FoodServiceTest.java
+++ src/test/java/com/fatsecret/platform/services/FoodServiceTest.java
@@ -1,40 +1,58 @@
 package com.fatsecret.platform.services;
 
-import static org.junit.Assert.assertEquals;
-
+import com.fatsecret.platform.model.CompactFood;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.fatsecret.platform.model.Food;
 
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class FoodServiceTest {
-	private FatsecretService service;
-	
-	@Before
-	public void setUp() {
+    private FatsecretService service;
+
+    @Before
+    public void setUp() {
         String key = "your key";
         String secret = "your secret";
 
         service = new FatsecretService(key, secret);
-	}
-	
-	@Test
-	public void testGetFood() {
-		Food food = service.getFood(285243L);
-		assertEquals("Penne", food.getName());
-		assertEquals(4, food.getServings().size());
-		assertEquals("Generic", food.getType());
-	}
+    }
 
-	@Test
-	public void testGetFoodTypeGeneric() {
-		Food food = service.getFood(285243L);
-		assertEquals("Generic", food.getType());
-	}
+    @Test
+    public void testGetFood() {
+        Food food = service.getFood(285243L);
+        assertEquals("Penne", food.getName());
+        assertEquals(4, food.getServings().size());
+        assertEquals("Generic", food.getType());
+    }
 
-	@Test
-	public void testGetFoodTypeBrand() {
-		Food food = service.getFood(1844450L);
-		assertEquals("Brand", food.getType());
-	}
+    @Test
+    public void testGetFoodTypeGeneric() {
+        Food food = service.getFood(285243L);
+        assertEquals("Generic", food.getType());
+    }
+
+    @Test
+    public void testGetFoodTypeBrand() {
+        Food food = service.getFood(1844450L);
+        assertEquals("Brand", food.getType());
+    }
+
+    @Test
+    public void brandNameIsSetForFoodSearch() {
+        List<CompactFood> results = service.searchFoods("toast").getResults();
+
+        int resultCountWithTypeBrand = 0;
+        for (CompactFood food : results) {
+            if (food.getType().equals("Brand")) {
+                resultCountWithTypeBrand++;
+                assertNotNull(food.getBrandName());
+            }
+        }
+
+        assertTrue(resultCountWithTypeBrand > 0);
+    }
 }
